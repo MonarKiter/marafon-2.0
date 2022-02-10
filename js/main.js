88a88cc5e00af03f349c6790546dbabbd9a4dc61
MonarKiter <monarkiter@yandex.ru>
2022-02-10 19:45:42 +0300

    completed section featured, creating section featured-video slider swiper

---
+++ js/main.js
@@ -0,0 +1,26 @@
+import './_vendor';
+import vars from './_vars';
+import './_functions';
+import './_components';
+
+// import Swiper JS
+import Swiper, { Navigation, Pagination } from 'swiper';
+//Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+		paginationClickable : true,
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
