6591d73b0e4e7d524725dbb942518f4d2b15bf12
MonarKiter <monarkiter@yandex.ru>
2022-02-13 15:04:31 +0300

    Добавлен второй слайдер, hover над кнопками, добавлен burger menu

--- js/main.js
+++ js/main.js
@@ -21,3 +21,4 @@ const swiper = new Swiper('.swiper', {
     },
 
 });
+
--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,44 @@ import './_functions';
 import './_components';
 
 // import Swiper JS
-import Swiper, { Navigation, Pagination } from 'swiper';
-//Swiper.use([Navigation, Pagination]);
+import Swiper, { Navigation, Pagination,EffectFade} from 'swiper';
+Swiper.use([Navigation, Pagination,EffectFade]);
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-		centeredSlides: true,
-		slidesPerView: 'auto',
-    loop: true,
-		spaceBetween: 30,
+
+const swiper1 = document.querySelector('.swiper'),
+			swiper2 = document.querySelector('.swiper-con'),
+			burger = document.querySelector('.burger'),
+			close = document.querySelector('.menu__close'),
+			menu = document.querySelector('.menu');
+
+const swiperSlider1 = new Swiper(swiper1, {
+	// Optional parameters
+	centeredSlides: true,
+	slidesPerView: 'auto',
+	loop: true,
+	spaceBetween: 30,
+});
+
+const swiperSlider2 = new Swiper(swiper2, {
+	// Optional parameters
+	fadeEffect: {crossFade: true},
+	centeredSlides: true,
+	slidesPerView: 1,
+	loop: true,
+	spaceBetween: 10,
+	effect: "fade",
+
+	navigation: {
+    nextEl: '.btn-right',
+    prevEl: '.btn-left',
+  },
+});
+
+
+burger.addEventListener('click', () => {
+	menu.classList.add('menu--visible');
+});
+
+close.addEventListener('click', () => {
+	menu.classList.remove('menu--visible');
 });
